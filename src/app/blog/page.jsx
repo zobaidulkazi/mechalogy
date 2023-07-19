f9c944678dde2ba5a259e3b0157c703f7ec433ff
zobaidulkazi <mdzobaidulislamkazi@gmail.com>
2023-07-20 03:32:54 +0600

    Mechalogy V2.02

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,11 +4,7 @@ const BlogPage = () => {
   return (
     <div className="bg-gray-100">
         <article className="overflow-hidden rounded-lg shadow transition hover:shadow-lg dark:shadow-gray-700/25">
-      <img
-        alt="Office"
-        src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
-        className="h-56 w-full object-cover"
-      />
+   
 
       <div className="bg-white p-4 dark:bg-gray-900 sm:p-6">
         <time
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -6,7 +6,7 @@ import { Axios } from 'axios'
 import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 
-export default function logInPage() {
+export default function LogInPage() {
 
 
     const [user, setUser] = useState({
